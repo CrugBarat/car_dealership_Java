b40af104250c9444ea7855299f911b36734a291d
CrugBarat <craigtbarratt@gmail.com>
2020-06-04 01:33:33 +0100

    adds Alloy subclass of Component with constructor
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/vehicle_components/Alloy.java
@@ -0,0 +1,8 @@
+package vehicle_components;
+
+public class Alloy extends Component {
+
+    public Alloy(String brand, String model, int quantity, int warranty) {
+        super(brand, model, quantity, warranty);
+    }
+}
